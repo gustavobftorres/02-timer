70f932706ab196d6d417cd4c558b7d30deba8bc1
GUSNEO <gutobevtorres@gmail.com>
2024-07-17 17:34:46 -0300

    styles for time counter

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,37 @@
+import { Play } from "phosphor-react";
+import { FormContainer, HomeContainer } from "./styles";
+import { CountDownContainer } from "./styles";
+import { Separator } from "./styles";
+
 export function Home() {
   return (
-    <div>
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task"></input>
 
-    </div>
+          <label htmlFor="task">durante</label>
+          <input id="minutesAmonut" type="number"></input>
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <button type="submit" >
+          <Play/>
+          Começar
+        </button>
+      </form>
+
+    </HomeContainer>
+
   )
 }
